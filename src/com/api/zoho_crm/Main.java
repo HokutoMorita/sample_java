67c98acdeba7da24e05615138d6adb3e67ebc7fa
Hokuto Morita <han2210mh@gmail.com>
2022-02-10 14:03:49 +0900

    環境変数の設定確認

--- src/com/api/zoho_crm/Main.java
+++ src/com/api/zoho_crm/Main.java
@@ -1,7 +1,14 @@
 package com.api.zoho_crm;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // 環境変数は、intellijの実行構成の編集上で追加すること(https://reasonable-code.com/intellij-environment-variable/)
+        System.out.println("CLIENT_ID: " + System.getenv("ZOHO_CLIENT_ID"));
+        System.out.println("CLIENT_SECRET: " + System.getenv("ZOHO_CLIENT_SECRET"));
+        System.out.println("REFRESH_TOKEN: " + System.getenv("ZOHO_REFRESH_TOKEN"));
+
     }
 }
