63ef7e65e78949a76d3791eb0a8d8a3d9e829a50
Hokuto Morita <han2210mh@gmail.com>
2022-02-10 14:31:13 +0900

    add

--- src/com/api/zoho_crm/Main.java
+++ src/com/api/zoho_crm/Main.java
@@ -1,13 +1,20 @@
 package com.api.zoho_crm;
 
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        logger.debug("Hello World");
         // 環境変数は、intellijの実行構成の編集上で追加すること(https://reasonable-code.com/intellij-environment-variable/)
         System.out.println("CLIENT_ID: " + System.getenv("ZOHO_CLIENT_ID"));
         System.out.println("CLIENT_SECRET: " + System.getenv("ZOHO_CLIENT_SECRET"));
+        System.out.println("REFRESH_TOKEN: " + System.getenv("ZOHO_REFRESH_TOKEN"));
+
+        // OAuth
+        String zohoAccountsUrl = "https://accounts.zoho.com";
+        String oauthUrl = String.format("%s/oauth/v2/token", zohoAccountsUrl);
         System.out.println("REFRESH_TOKEN: " + System.getenv("ZOHO_REFRESH_TOKEN"));
 
     }
